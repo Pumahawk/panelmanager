806ec95ee53e8bef8bfeb40e1cac97c270093c6c
Lorenzo Gandino <lorenzoga94@gmail.com>
2018-02-18 14:37:05 +0100

    Aggiunti collegamenti al sidebar delle pagine media

--- frontend/src/components/SideBar.vue
+++ frontend/src/components/SideBar.vue
@@ -38,6 +38,28 @@
           </router-link>
         </li>
         <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+          <span>Media</span>
+          <a class="d-flex align-items-center text-muted" href="#">
+            <span data-feather="plus-circle"></span>
+          </a>
+        </h6>
+        <li class="nav-item">
+          <router-link to="/">
+            <a class="nav-link" href="#">
+              <span data-feather="shopping-cart"></span>
+              Elenco media
+            </a>
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/">
+            <a class="nav-link" href="#">
+              <span data-feather="shopping-cart"></span>
+              Upload media
+            </a>
+          </router-link>
+        </li>
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
           <span>Fatture</span>
           <a class="d-flex align-items-center text-muted" href="#">
             <span data-feather="plus-circle"></span>
