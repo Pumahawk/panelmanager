9a80a1fdb8e1e7a0c332f92cdca598a319c0e416
Lorenzo Gandino <lorenzoga94@gmail.com>
2018-02-19 15:05:01 +0100

    Creata interfaccia per la pagina media upload

--- frontend/src/components/pages/MediaUpload.vue
+++ frontend/src/components/pages/MediaUpload.vue
@@ -1,12 +1,53 @@
 <template lang="html">
-  <div class="">
-    MediaUploadPage
+  <div>
+    <h1>Upload media</h1>
+    <hr>
+    <button class="btn btn-success">Upload</button>
+    <hr>
+    <div class="form">
+      <div class="form-group">
+        <label for="project"><strong>Progetto</strong></label>
+        <select class="form-control" name="">
+          <option v-for="progetto in progetti" :value="progetto.id">{{progetto.nome}}</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="media"><strong>Media</strong></label>
+        <input @change="updateForm()" id="media" type="file" class="form-control">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ProgettoRequest from '@/request/Progetto'
+
 export default {
-  name: "MediaUploadPage"
+  name: "MediaUploadPage",
+  data() {
+    return {
+      progetti: [],
+      form: {
+        file: null,
+        project: null
+      }
+    }
+  },
+  beforeCreate() {
+    var vuec = this;
+    ProgettoRequest.getAll(function(resp){
+      if(resp.data.status === 'OK') {
+        vuec.progetti = resp.data.data;
+      } else {
+        console.error(resp.data.message);
+      }
+    });
+  },
+  methods: {
+    updateForm() {
+      this.form.file = document.getElementById('media').files[0];
+    }
+  }
 }
 </script>
 
